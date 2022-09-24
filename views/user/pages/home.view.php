bd441f43935070e7a5831090d367c9186d355822
ValuONE <vapiendl@gmail.com>
2022-09-25 01:08:34 +0200

    [+] Created home.view.php file

---
+++ views/user/pages/home.view.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Copyright (c) 2022 by Valu. All rights reserved
+ *
+ * @author Valu
+ */
+?>
+
+<div class="container">
+    <div class="slide slide-1">
+        <div class="caption">
+            <h3></h3>
+            <p></p>
+        </div>
+    </div>
+    <div class="slide slide-2">
+        <div class="caption">
+            <h3></h3>
+            <p></p>
+        </div>
+    </div>
+    <div class="slide slide-3">
+        <div class="caption">
+            <h3></h3>
+            <p></p>
+        </div>
+    </div>
+</div>
+<a id="prev" class="control prev" onclick="prev()"></a>
+<a id="next" class="control next" onclick="next()"></a>
