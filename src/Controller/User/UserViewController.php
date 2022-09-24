f2ec5dac73479498bbbd63fc21448ff0f173548a
ValuONE <vapiendl@gmail.com>
2022-09-25 01:10:16 +0200

    [/] Edited UserViewController.php file

--- src/Controller/User/UserViewController.php
+++ src/Controller/User/UserViewController.php
@@ -7,9 +7,29 @@
 
 namespace Valu\App\Controller\User;
 
+use Valu\App\Controller\AbstractController;
 use Valu\App\Support\Content\ContentHandler;
 
-class UserViewController
+class UserViewController extends AbstractController
 {
     public function __construct(protected ContentHandler $contentHandler) {}
+
+    public function view(): void
+    {
+        $data = null;
+
+        $this->contentHandler->ensureSession();
+
+        if (isset($_GET['id'])) {
+            $id = @(int) ($_GET['id'] ?? '');
+
+            if (!empty($id)) {
+                $data = $this->contentHandler->fetchBlogById($id);
+            }
+        }
+
+        $this->renderUser('pages/view', [
+            'data' => $data
+        ]);
+    }
 }
