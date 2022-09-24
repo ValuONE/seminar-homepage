6759d712bed5d1b30c5b4932643c40dc18de95f6
ValuONE <vapiendl@gmail.com>
2022-09-25 01:09:45 +0200

    [/] Edited UserAddController.php file

--- src/Controller/User/UserAddController.php
+++ src/Controller/User/UserAddController.php
@@ -7,10 +7,39 @@
 
 namespace Valu\App\Controller\User;
 
+use Valu\App\Controller\AbstractController;
 use Valu\App\Support\Content\ContentHandler;
 
-class UserAddController
+class UserAddController extends AbstractController
 {
     public function __construct(protected ContentHandler $contentHandler) {}
+
+    public function add()
+    {
+        $error = null;
+
+        if (!empty($_POST)) {
+            $title = @(string) ($_POST['title'] ?? '');
+            $content = @(string) ($_POST['content'] ?? '');
+            $file = ($_FILES['file'] ?? '');
+
+            if (!empty($title) && !empty($content) && !empty($file)) {
+                $result = $this->contentHandler->addBlog($title, $content, $file);
+
+                if ($result) {
+                    header('Location: ./?route=blog');
+                    die();
+                } else {
+                    $error = true;
+                }
+            } else {
+                $error = true;
+            }
+        }
+
+        $this->renderUser('pages/add', [
+            'error' => $error
+        ]);
+    }
 
 }
