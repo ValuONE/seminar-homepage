7429ff26bf8cd3e3cbd9a09ee51655e00d8f59a6
ValuONE <vapiendl@gmail.com>
2022-09-26 19:53:24 +0200

    [+] Created limiter.js file

---
+++ assets/js/limiter.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("input[type='submit']").click(function(){
+        const $fileUpload = $("input[type='file']");
+        if (parseInt($fileUpload.get(0).files.length) !== 5){
+            alert("Es müssen genau 5 Dateien hoch geladen werden!");
+        }
+    });
+});
