b0988575fc3c9b6621bf95002a7cad5ac002a661
ValuONE <vapiendl@gmail.com>
2022-09-26 19:54:06 +0200

    [/] Edited vote.view.php file

--- views/user/pages/vote.view.php
+++ views/user/pages/vote.view.php
@@ -9,33 +9,11 @@
 <form method="post" action="./?route=vote" enctype="multipart/form-data">
 
     <div class="img-container">
-        <input id="img1" type="file" name="file[]" onchange="loadFile('img1', 'img1-preview')"><br>
-        <img class="img-preview" id="img1-preview" src="#" alt="Kein Bild geladen">
-    </div>
+        <input id="gallery-photo-add" type="file" name="file[]" multiple><br>
+        <div class="gallery">
 
-    <div class="img-container">
-        <input id="img2" type="file" name="file[]"><br>
-        <img class="img-preview" id="img2-preview" src="#" alt="Kein Bild geladen">
+        </div>
     </div>
-
-    <div class="img-container">
-        <input id="img3" type="file" name="file[]"><br>
-        <img class="img-preview" id="img3-preview" src="#" alt="Kein Bild geladen">
-    </div>
-
-    <div class="img-container">
-        <input id="img4" type="file" name="file[]"><br>
-        <img class="img-preview" id="img4-preview" src="#" alt="Kein Bild geladen">
-    </div>
-
-    <div class="img-container">
-        <input id="img5" type="file" name="file[]"><br>
-        <img class="img-preview" id="img5-preview" src="#" alt="Kein Bild geladen">
-    </div>
-
-    <label>
-        Ich bestätige, dass alle ausgewählten Bilder meine eigenen sind:
-        <input type="checkbox" name="checkbox" required>
-    </label>
     <input type="submit" name="upload">
 </form>
+<script src="../../../assets/js/limiter.js"></script>
