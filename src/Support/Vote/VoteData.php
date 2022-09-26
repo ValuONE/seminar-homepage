47b9bd634ccc93aa88c73c23b80dc0b3c2eb08a3
ValuONE <vapiendl@gmail.com>
2022-09-26 13:23:40 +0200

    [+] Created VoteData.php file

---
+++ src/Support/Vote/VoteData.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Copyright (c) 2022 by Valu. All rights reserved
+ *
+ * @author Valu
+ */
+
+namespace Valu\App\Support\Vote;
+
+class VoteData
+{
+    public int $id;
+    public string $author;
+    public string $filename;
+    public int $votes;
+}
