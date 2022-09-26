b5e589c7ffa38beb4642f146472ad9d1c485c51b
ValuONE <vapiendl@gmail.com>
2022-09-26 13:22:38 +0200

    [/] Edited index.php file

--- index.php
+++ index.php
@@ -10,22 +10,26 @@ use Valu\App\Controller\User\UserBlogController;
 use Valu\App\Controller\User\UserHomeController;
 use Valu\App\Controller\User\UserVerifyController;
 use Valu\App\Controller\User\UserViewController;
+use Valu\App\Controller\User\UserVoteController;
 use Valu\App\Support\Authentication\AuthService;
 use Valu\App\Controller\User\UserLoginController;
 use Valu\App\Support\Content\ContentHandler;
+use Valu\App\Support\Vote\VoteHandler;
 
 require_once __DIR__ . '/inc/all.inc.php';
 
 $authService = new AuthService($pdo);
 $contentHandler = new ContentHandler($pdo);
+$voteHandler = new VoteHandler($pdo);
 $userLoginController = new UserLoginController($authService);
 $userVerifyController = new UserVerifyController($authService);
 $userAddController = new UserAddController($contentHandler);
 $userBlogController = new UserBlogController($contentHandler);
 $userViewController = new UserViewController($contentHandler);
 $userHomeController = new UserHomeController($contentHandler);
+$userVoteController = new UserVoteController($voteHandler);
 
-$route = @(string) ($_GET['route'] ?? 'home');
+$route = @(string) ($_GET['route'] ?? 'vote');
 
 switch ($route) {
     case ('home'): {
@@ -34,6 +38,10 @@ switch ($route) {
     }
     case ('blog'): {
         $userBlogController->blog();
+        break;
+    }
+    case ('vote'): {
+        $userVoteController->vote();
         break;
     }
     case ('login'): {
