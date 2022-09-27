f60c91712d479971df0a4af597b0c2d05f5c8a0d
ValuONE <vapiendl@gmail.com>
2022-09-27 18:35:35 +0200

    [/] Edited limiter.js file

--- assets/js/limiter.js
+++ assets/js/limiter.js
@@ -1,5 +1,5 @@
 $(function(){
-    $("input[type='submit']").click(function(){
+    $(".submit").click(function(){
         const $fileUpload = $("input[type='file']");
         if (parseInt($fileUpload.get(0).files.length) !== 5){
             alert("Es müssen genau 5 Dateien hoch geladen werden!");
